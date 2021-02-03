d906b59fdd210914d9b8f2c04e18b3f728778eea
michaelcordero <michaelpetercordero@gmail.com>
2021-02-03 02:31:17 -0500

    * Corrected and tested insert method for SortedArrayBufferWithDups * Tomorrow I'll finish the other methods.

--- src/SortedArrayBufferWithDups.java
+++ src/SortedArrayBufferWithDups.java
@@ -13,28 +13,26 @@ public class SortedArrayBufferWithDups extends ArrayBufferWithDups{
         }
         // Array Buffer not full case
         if (numberOfElements != intArray.length) {
-            // let's check for duplicates
-            int location = locationOf(value);
-            if (location != -1) {
-                // we have a duplicate, shift next element up, to make room
-                intArray[location + 2] = intArray[location + 1];
-                // now store value
-                intArray[location + 1] = value;
+            if (value > intArray[numberOfElements - 1 ]) {
+                intArray[numberOfElements] = value;
                 numberOfElements++;
                 return true;
-            } else {
-                // no duplicates so follow normal logic
-                if (value > intArray[numberOfElements - 1 ]) {
-                    intArray[numberOfElements] = value;
-                    numberOfElements++;
-                    return true;
-                } else if (value < intArray[numberOfElements - 1]) {
-                    // shift up
-                    intArray[numberOfElements] = intArray[numberOfElements - 1];
-                    intArray[numberOfElements - 1] = value;
-                    numberOfElements++;
-                    return true;
+            } else if (value < intArray[numberOfElements - 1]) {
+                // find destination index
+                int destination = 0;
+                for (int i = 0; i < numberOfElements - 1; i++) {
+                    if (value > intArray[i]) {
+                        destination = i + 1;
+                    }
                 }
+                // shift elements down
+                for (int i = numberOfElements - 1; i >= destination; i--) {
+                    intArray[i+1] = intArray[i];
+                }
+                // finally insert element
+                intArray[destination] = value;
+                numberOfElements++;
+                return true;
             }
         }
         // Array Buffer is full
