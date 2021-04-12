2342378e432ba2081a1d50a4fcf34a3fd9b54f41
michaelcordero <michaelpetercordero@gmail.com>
2021-04-12 00:35:55 -0400

    * cleanup

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
-public class WeightedGraph<K extends Comparable<K>,V> implements Graph<K,V> {
+public class WeightedGraph<K extends Comparable<K>, V> implements Graph<K, V> {
     ///////////////////////////////////////////////
     // properties
     //////////////////////////////////////////////
-    private final Map<K,GraphVertex<K,V>> verticesMap;
+    private final Map<K, GraphVertex<K, V>> verticesMap;
 
     ///////////////////////////////////////////////
     // constructors
@@ -16,13 +16,14 @@ public class WeightedGraph<K extends Comparable<K>,V> implements Graph<K,V> {
     ///////////////////////////////////////////////
     // inner vertex class
     //////////////////////////////////////////////
-    protected static class WeightedGraphVertex<K extends Comparable<K>,V> implements GraphVertex<K,V> {
+    protected static class WeightedGraphVertex<K extends Comparable<K>, V> implements GraphVertex<K, V> {
         ///////////////////////////////////////////////
         // properties
         //////////////////////////////////////////////
         K key;
         V value;
-        Map<GraphVertex<K,V>,Integer> adjacencyMap;
+        Map<GraphVertex<K, V>, Integer> adjacencyMap;
+
         ///////////////////////////////////////////////
         // constructors
         //////////////////////////////////////////////
@@ -84,14 +85,14 @@ public class WeightedGraph<K extends Comparable<K>,V> implements Graph<K,V> {
 
     @Override
     public void addVertex(K key, V value) {
-        GraphVertex<K,V> vertex = new WeightedGraphVertex<>(key, value);
+        GraphVertex<K, V> vertex = new WeightedGraphVertex<>(key, value);
         verticesMap.put(key, vertex);
     }
 
     @Override
     public void addEdge(K fromVertexKey, K toVertexKey, int edge) {
-        GraphVertex<K,V> from = verticesMap.get(fromVertexKey);
-        GraphVertex<K,V> to = verticesMap.get(toVertexKey);
+        GraphVertex<K, V> from = verticesMap.get(fromVertexKey);
+        GraphVertex<K, V> to = verticesMap.get(toVertexKey);
         // undirected is default, so bidirectional edges occur.
         from.addEdge(to, edge);
         to.addEdge(from, edge);
@@ -99,9 +100,9 @@ public class WeightedGraph<K extends Comparable<K>,V> implements Graph<K,V> {
 
     @Override
     public void displayAdjacencyLists() {
-        verticesMap.forEach( (key,vertex) -> {
+        verticesMap.forEach((key, vertex) -> {
             System.out.print("Vertex " + key + ": ");
-            vertex.adjacencyList().forEach( v -> System.out.print("("+ key + "," + v.getKey() + ")"));
+            vertex.adjacencyList().forEach(v -> System.out.print("(" + key + "," + v.getKey() + ")"));
             System.out.println();
         });
     }
@@ -109,32 +110,36 @@ public class WeightedGraph<K extends Comparable<K>,V> implements Graph<K,V> {
     /**
      * This shortest path method uses Dijkstra's famous algorithm for finding the shortest path from one vertex to
      * another vertex, by using a non-negative weighted graph.
+     * Credits: (Back to Back SWE)[https://www.youtube.com/watch?v=K_1urzWrzLs]
+     *
      * @param fromVertexKey - the starting vertex
-     * @param toVertexKey - the ending vertex
+     * @param toVertexKey   - the ending vertex
      */
     @Override
     public void shortestPath(K fromVertexKey, K toVertexKey) {
         // Create solution Table
-        Map<GraphVertex<K,V>,DijkstraTable<K,V>> solution_table = new TreeMap<>();
+        Map<GraphVertex<K, V>, DijkstraTable<K, V>> solution_table = new TreeMap<>();
         // Initialize all values to Infinity
-        verticesMap.values().forEach(v -> solution_table.put(v, new DijkstraTable<>(v,Integer.MAX_VALUE,null)));
+        verticesMap.values().forEach(v -> solution_table.put(v, new DijkstraTable<>(v, Integer.MAX_VALUE, null)));
         // Change start vertex distance to zero
-        GraphVertex<K,V> start = verticesMap.get(fromVertexKey);
+        GraphVertex<K, V> start = verticesMap.get(fromVertexKey);
         assert start != null;
-        solution_table.put(start, new DijkstraTable<>(start,0, start));
+        solution_table.put(start, new DijkstraTable<>(start, 0, start));
         // Declare minimum priority queue and populate with all values for processing
-        PriorityQueue<DijkstraTable<K,V>> pq = new PriorityQueue<>(this.size(), Comparator.comparing(DijkstraTable::getDistance));
+        PriorityQueue<DijkstraTable<K, V>> pq = new PriorityQueue<>(this.size(),
+                Comparator.comparing(DijkstraTable::getDistance));
         solution_table.values().forEach(pq::offer);
         // now let's begin the algorithm
-        while(!pq.isEmpty()) {
-            DijkstraTable<K,V> current = pq.poll();
+        while (!pq.isEmpty()) {
+            DijkstraTable<K, V> current = pq.poll();
             // examine distances of all the adjacent nodes
             current.getVertex().adjacencyList().forEach(v -> {
                 int to_weight = v.getEdge(current.getVertex());
                 int current_distance = solution_table.get(v).getDistance();
-                // when any positive int is added to Integer.MAX_VALUE, it ends up being negative.
-                // to fix this bug, we don't change the value and leave as is.
-                int computed_distance = current.getDistance() + to_weight < 0 ? current.getDistance() : current.getDistance() + to_weight ;
+                // when any positive int is added to Integer.MAX_VALUE, it overflows and results in a negative value.
+                // to fix this, we don't change the value and leave as is.
+                int computed_distance = current.getDistance() + to_weight < 0 ? current.getDistance()
+                        : current.getDistance() + to_weight;
                 if (computed_distance < current_distance) {
                     // write new values to solution table
                     solution_table.get(v).setDistance(computed_distance);
@@ -145,19 +150,19 @@ public class WeightedGraph<K extends Comparable<K>,V> implements Graph<K,V> {
         // values marked in solutions table, so now let's back track from the destination node to the start node.
         // adding the parent vertices along the way.
         // using a stack because we want the order to be reversed.
-        Stack<GraphVertex<K,V>> path_vertices = new Stack<>();
-        GraphVertex<K,V> destination_node = verticesMap.get(toVertexKey);
-        DijkstraTable<K,V> destination_table = solution_table.get(destination_node);
-        GraphVertex<K,V> parent = destination_table.getVertex();
+        Stack<GraphVertex<K, V>> path_vertices = new Stack<>();
+        GraphVertex<K, V> destination_node = verticesMap.get(toVertexKey);
+        DijkstraTable<K, V> destination_table = solution_table.get(destination_node);
+        GraphVertex<K, V> parent = destination_table.getVertex();
         path_vertices.add(parent);
-        while (parent != null && parent != ((TreeMap<K,V>)solution_table).firstKey()) {
-            DijkstraTable<K,V> current = solution_table.get(parent);
+        while (parent != null && parent != ((TreeMap<K, V>) solution_table).firstKey()) {
+            DijkstraTable<K, V> current = solution_table.get(parent);
             parent = current.getParent();
             path_vertices.add(parent);
         }
         System.out.print("The path is: ");
         while (!path_vertices.empty()) {
-            GraphVertex<K,V> current = path_vertices.pop();
+            GraphVertex<K, V> current = path_vertices.pop();
             String arrow = !path_vertices.empty() ? " -> " : "";
             System.out.print(current.getKey() + arrow);
         }
